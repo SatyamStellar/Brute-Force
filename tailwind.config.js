d4c5eebd563b7ecb714c5d105d1092ddaefce5a5
Satyam Tomar <satyam321tomar@gmail.com>
2024-09-27 21:23:12 +0530

    adding my brute force project to deploy

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ['JetBrains Mono']
+      },
+      colors: {
+        'prigreen': "#86FF84"
+      }
+    },
+  },
+  plugins: [],
+}
+
